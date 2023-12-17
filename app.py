974270de6b25616926f084ab2c1513dc020daf08
widawek <kolczywiewicz@gmail.com>
2023-12-17 17:35:09 +0100

    Add flask solutions to generate templates

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return redirect("me")
+
+
 @app.route("/me")
 def me():
-    return render_template("me.html")
+    content = {
+        "title": "O mnie",
+        "name": "Dawid",
+        "surname": "Okrojek"
+    }
+    main_title = "informacje o mnie"
+    return render_template("me.html", content=content, main_title=main_title)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    content = {
+        "title": "Kontakt",
+        "email": "email: dawid.okrojek@gmail.com",
+        "phone": "telefon: 700-600-500"
+    }
+    main_title = "informacje kontaktowe i zapis formularza"
+    return render_template("contact.html", content=content,
+                           main_title=main_title)
 
 
 if __name__ == "__main__":
